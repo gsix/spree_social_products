64db2be81a789e5a241b09279b2a10fa43327950
Denis Gabdrakhmanov <denisboiling@gmail.com>
2014-05-24 21:50:03 +0300

    fix vk

--- app/models/app_configuration_decorator.rb
+++ app/models/app_configuration_decorator.rb
@@ -9,12 +9,12 @@ Spree::AppConfiguration.class_eval do
   preference :delicious_button, :boolean, :default => false
   preference :reddit_button, :boolean, :default => false
 
+  preference :vkontakte_app_id, :string
+
   preference :facebook_app_id, :string
   preference :facebook_layout, :string, :default => 'standard'
   preference :facebook_show_faces, :boolean, :default => false
   preference :facebook_verb_to_display, :string, :default => 'like'
   preference :facebook_color_scheme, :string, :default => 'light'
   preference :facebook_send_button, :boolean, :default => false
-
-  preference :vkontakte_app_id, :string
 end
